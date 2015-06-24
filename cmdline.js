1846493832ee5f26e1329b2b1197b9a75a6f9c74
MnsA <manala@edu.umea.se>
2015-06-24 10:26:05 +0200

    added cmdline

--- cmdline.js
+++ cmdline.js
@@ -1,11 +1,26 @@
 $(window).ready(function() {
 	$('.hidden-item').hide();
 	fixTextSize();
+	
+	$('#search-bar').keyup(function(evt) {
+		if(evt.key == 'Enter' || evt.keyCode == 13) {
+			runCommand($(this).val());
+			$(this).val('');
+		}
+	});
 });
 
 function runCommand(cmd) {
 	var words = cmd.split(/\s/);
-	$(words[0]).show(500);
+	
+	switch(words[0]) {
+	case 'show':
+		$('#item-' + words[1]).show(500);
+		break;
+	case 'hide':
+		$('#item-' + words[1]).hide(500);
+		break;
+	}
 }
 
 function fixTextSize() {
