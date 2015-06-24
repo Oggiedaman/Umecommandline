fb2ae9bcb4b303e0fd51d70e5aa83defee558f88
MnsA <manala@edu.umea.se>
2015-06-24 10:01:31 +0200

    updated style

--- cmdline.js
+++ cmdline.js
@@ -1,8 +1,14 @@
 $(window).ready(function() {
 	$('.hidden-item').hide();
+	fixTextSize();
 });
 
 function runCommand(cmd) {
 	var words = cmd.split(/\s/);
 	$(words[0]).show(500);
+}
+
+function fixTextSize() {
+	var elem = $('#search-bar');
+	elem.css('font-size', elem.height() * 0.9 + 'px');
 }
